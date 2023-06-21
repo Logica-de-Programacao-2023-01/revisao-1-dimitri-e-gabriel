228661375e86a4edea0b8b4ca86ae3c76d337e0b
Cinnanti44 <126252524+Cinnanti44@users.noreply.github.com>
2023-06-20 21:20:58 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -1,6 +1,23 @@
 package q3
 
 func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
-	// Implemente sua solução aqui
-	return 0, 0, 0, nil
+		min := numbers[0]
+	max := numbers[0]
+	sum := 0
+
+	for _, num := range numbers {
+		if num < min {
+			min = num
+		}
+		if num > max {
+			max = num
+		}
+		sum += num
+	}
+
+	average := float64(sum) / float64(len(numbers))
+
+	return min, max, average, nil
 }
+
+
