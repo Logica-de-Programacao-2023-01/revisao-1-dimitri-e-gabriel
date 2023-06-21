f4c0d5f40a8d7259aa1753038e548e611984bfd6
Cinnanti44 <126252524+Cinnanti44@users.noreply.github.com>
2023-06-20 21:20:27 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -1,6 +1,38 @@
 package q4
 
 func CalculateFinalPrice(basePrice float64, state string, taxCode int) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	if basePrice <= 0 {
+		return 0, errors.New("preço base inválido")
+	}
+
+	var taxRate float64
+	switch taxCode {
+	case 1:
+		taxRate = 0.05
+	case 2:
+		taxRate = 0.10
+	case 3:
+		taxRate = 0.15
+	default:
+		return 0, errors.New("imposto não encontrado")
+	}
+
+	var freightRate float64
+	switch state {
+	case "SP":
+		freightRate = 0.10
+	case "RJ":
+		freightRate = 0.15
+	case "MG":
+		freightRate = 0.20
+	case "ES":
+		freightRate = 0.25
+	default:
+		freightRate = 0.30
+	}
+
+	finalPrice := basePrice + (basePrice * taxRate) + (basePrice * freightRate)
+	return finalPrice, nil
 }
+
+
