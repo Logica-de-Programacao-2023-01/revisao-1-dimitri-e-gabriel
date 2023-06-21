6f5ed3ed32f7225407f82a4e8adf15f34eb304ac
Cinnanti44 <126252524+Cinnanti44@users.noreply.github.com>
2023-06-20 21:21:36 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,27 @@
 package q2
 
 func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+words := strings.Fields(text)
+	numWords := len(words)
+
+	if numWords == 0 {
+		return 0, errors.New("texto vazio")
+	}
+
+	totalLetters := 0
+	for _, word := range words {
+		totalLetters += len(word)
+	}
+
+	average := float64(totalLetters) / float64(numWords)
+	return average, nil
+}
+
+func main() {
+	text := "O rato roeu a roupa do rei de Roma"
+	average, err := AverageLettersPerWord(text)
+	if err != nil {
+		panic(err)
+	}
+	println("Média de letras por palavra:", average)
 }
